d761b64da86dbb9f58b0cdc4c27b40172cd5a80e
Huangchuting <759040635@qq.com>
2020-02-10 14:27:21 +0800

    fix:集成路径出错2

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -3,7 +3,7 @@ describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base')
     // console.log(baseConfig)
     it('entry', () => {
-        assert.equal(baseConfig.entry.index.indexOf('//builder-webpack/test/smoke/template/src/entry/index/index.js') > -1, true)
+        assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/entry/index/index.js') > -1, true)
         assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/entry/search/index.js') > -1, true)
     })
 })
